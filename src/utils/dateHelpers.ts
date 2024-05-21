a08671857c05ae59d59198af274fa41351c192e9
Gantumur Dalaijamts <dalai@Gantumurs-MacBook-Air.local>
2024-05-21 13:44:03 +0800

    da

---
+++ src/utils/dateHelpers.ts
@@ -0,0 +1,14 @@
+import { DateTime } from 'luxon'
+
+function now(): Date {
+    return new Date(
+        `${DateTime.local()
+            .toISO()
+            .substr(0, 23)}Z`
+    )
+}
+
+
+export default {
+    now
+}
