21734d1144636dec4f2a01d7d072e3bcaa11c0c7
Gantumur Dalaijamts <dalai@Gantumurs-MacBook-Air.local>
2024-05-21 15:04:28 +0800

    da;a

--- src/functions.ts
+++ src/functions.ts
@@ -3,5 +3,6 @@ import { sayHelloType } from "./types";
 export function sayHello({firstname, lastname}:sayHelloType){
     console.log(firstname+"+"+lastname);
 
+    console.log("gisasdas")
     console.log(process.env.MY_ENV_VAR)
 }
